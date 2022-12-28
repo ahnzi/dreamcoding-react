5222a3fbca4fe1a15162c45fad0508a9611ebd38
Ahnzi <jhhh0627@gmail.com>
2022-12-28 09:12:06 +0900

    #4.8 Profile 첫 컴포넌트 만들기

--- basic/src/components/Profile.jsx
+++ basic/src/components/Profile.jsx
@@ -2,8 +2,14 @@ import React from 'react';
 
 export default function Profile() {
     return (
-        <div>
-            
+        <div className='profile'>
+            <img
+                className='photo' 
+                src="https://images.unsplash.com/photo-1664575600850-c4b712e6e2bf?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1887&q=80" 
+                alt="avatar"
+            />
+            <h1>James Kim</h1>
+            <p>프론트앤드 개발자</p>
         </div>
     );
 }
